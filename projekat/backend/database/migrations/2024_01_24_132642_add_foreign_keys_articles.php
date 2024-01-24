7aff83a759f3850775a4df4bf9fc07eece493b03
Lav Jovanovic <lavjov@gmail.com>
2024-01-24 14:35:03 +0100

    dodate migracije za spoljne kljuceve

---
+++ projekat/backend/database/migrations/2024_01_24_132642_add_foreign_keys_articles.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            
+
+            $table->foreign('author_id')->references('author_id')->on('authors');
+            $table->foreign('category_id')->references('category_id')->on('categories');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+};
