df8d80e0a38ea04e3249d520ba23ea5cde0ede2c
Lav Jovanovic <lavjov@gmail.com>
2024-01-28 22:48:59 +0100

    dodata metoda update

--- projekat/backend/app/Http/Controllers/ArticleController.php
+++ projekat/backend/app/Http/Controllers/ArticleController.php
@@ -8,62 +8,74 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    
-    public function index(){
+
+    public function index()
+    {
 
         $articles = Article::all();
 
-        if(!$articles){
+        if (!$articles) {
 
-            return response()->json(['status' => 'Neuspeh','poruka'=>'Ne postoje clanci u sistemu!'],404);
+            return response()->json(['status' => 'Neuspeh', 'poruka' => 'Ne postoje clanci u sistemu!'], 404);
         }
 
-        return response()->json(['status' => 'Uspesan','clanci'=>$articles],200);
-
+        return response()->json(['status' => 'Uspesan', 'clanci' => $articles], 200);
     }
 
-    public function store(Request $request){
+    // kreira se jedan clanak
+    public function store(Request $request)
+    {
 
         $request->validate([
-            'title'=> 'required|string',
-            'content'=>'required|string',
-            'publishing_date'=>'required|date',
-            'author_id'=>'exists:authors,id',
-            'category_id'=>'exists:categories,id'
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'publishing_date' => 'required|date',
+            'author_id' => 'exists:authors,id',
+            'category_id' => 'exists:categories,id'
 
         ]);
 
-// kreira se jedan clanak
+
 
         $article = Article::create([
 
-            'title'=>$request->title,
-            'content'=>$request->content,
-            'publishing_date'=>$request->publishing_date,
-            'author_id'=>$request->author_id,
-            'category_id'=>$request->category_id
+            'title' => $request->title,
+            'content' => $request->content,
+            'publishing_date' => $request->publishing_date,
+            'author_id' => $request->author_id,
+            'category_id' => $request->category_id
         ]);
 
-        return response()->json(['status'=>'Uspesan','clanci'=>$article],201);
+        return response()->json(['status' => 'Uspesan', 'clanci' => $article], 201);
     }
 
 
-//prikazuje se odredjeni clanak
+    //prikazuje se odredjeni clanak
 
-    public function show($id){
+    public function show($id)
+    {
 
-        $article = Article::where('article_id',$id)->first();
+        $article = Article::where('article_id', $id)->first();
 
-        if(!$article){
+        if (!$article) {
 
-            return response()->json(['status' => 'Neuspesan', 'poruka'=> 'Ne postoji takav clanak u sistemu!'],404);
-
+            return response()->json(['status' => 'Neuspesan', 'poruka' => 'Ne postoji takav clanak u sistemu!'], 404);
         }
 
-        return response()->json(['status'=>'Uspesan','article' => $article],200);
-
+        return response()->json(['status' => 'Uspesan', 'article' => $article], 200);
     }
 
-    
 
+    public function update(Request $request, $id)
+    {
+
+        $article = Article::where('articles', $id)->first();
+
+        if (!$article) {
+
+            return response()->json(['status' => 'Neuspesan', 'poruka' => 'Ne postoji takav clanak u sistemu!'], 404);
+        }
+
+        
+    }
 }
