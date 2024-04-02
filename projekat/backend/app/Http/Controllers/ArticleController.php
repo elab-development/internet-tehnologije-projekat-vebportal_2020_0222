22ffa7fb0a47cb5663fe3cad87299cec6cb4ccae
doktoree <tjuring901@gmail.com>
2024-04-02 18:08:15 +0200

    Promena u ArticleControlleru

--- projekat/backend/app/Http/Controllers/ArticleController.php
+++ projekat/backend/app/Http/Controllers/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends Controller
     public function index()
     {
 
-        $articles = Article::all();
+        $articles = Article::paginate(10);
 
         if ($articles->isEmpty()) {
 
